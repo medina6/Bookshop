f264e22f151baee1f73b2b06b1e55bf6e6010665
medina6 <medinakubanych@gmail.com>
2022-04-02 13:31:21 +0600

    done

---
+++ order/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from .models import Order
+
+class CreateOrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = '__al__'
+
+    def clean_phone(self):
+        data = self.cleaned_data
+        phone = data.get('phone')
+        print(phone)
+        if phone.startswith('+996'):
+            raise forms.ValidationError('Number shoul start with +996')
+        if len(phone) != 13:
+            raise forms.ValidationError('Invalid phone Number')
+        return phone
+
